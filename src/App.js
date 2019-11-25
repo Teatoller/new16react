fb8b4737f6d1d1fe4d9ba692d036444e3ea916b9
Teatoller <steven.otieno1@gmail.com>
2019-11-25 07:10:17 +0300

    (class): Implement a class component
    - change app to class component
    - set app state and add didcatch life cycle
    - add conditional to display error else render application
    
    [Starts #003]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import StudentForm from "./components/StudentForm";
 
-function App() {
-  return (
-    <div className="App">
-      <StudentForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false
+    };
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>"Error Message: Something went wrong</h1>;
+    } else {
+      return (
+        <div className="App">
+          <StudentForm />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
